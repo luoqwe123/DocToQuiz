f77841827d49daedea03b9cf33d326087c5f944c
风风 <qwe18173945756@163.com>
2025-07-15 16:21:15 +0800

    修改并测试了eslint配置

--- eslint.config.js
+++ eslint.config.js
@@ -6,8 +6,17 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"], languageOptions: { globals: globals.browser } },
+  { files: ["src/*.{js,mjs,cjs,ts,vue}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["src/*.{js,mjs,cjs,ts,vue}"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      indent: ["error", 4], // 缩进 2 个空格
+      semi: ["error", "always"], // 强制分号
+      "no-console": ["error"], // 允许 .js 文件中使用 console
+      "no-require": "off", // 允许使用 require（针对 JS 文件）
+    },
+  },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="App-container">
-        
+
     </div>
 </template>
 
 <script setup lang='ts'>
 
-    console.log(111)
+console.log(111);
 
 </script>
 
 <style scoped>
 
-</style >
+
+</style>
